333054d49cdf33f625630f44247aa1a1919d5070
StenTechDev <stentechdev@gmail.com>
2020-12-26 11:07:40 +0000

    init

---
+++ nameCompatibilityCalculator.js
@@ -0,0 +1,8 @@
+alert("Welcome to the Name Compatibility Calculator 2020");
+prompt("Please Enter Your Name: ");
+prompt("Please Enter the Name You Want To Compare: ");
+
+var nameComp = Math.random() * 100;
+nameComp = Math.floor(nameComp) + 1;
+
+alert("Your Name Compatibility is " + nameComp + "%");
